a0781e026134ea23f58c6b713972eccd9b790bf0
Tobias Abbenhaus <hello@tbias25.de>
2022-05-30 16:59:43 +0200

    added UroriaNetwork.java, plugin.yml

---
+++ src/main/java/de/tbias25/urorianetwork/UroriaNetwork.java
@@ -0,0 +1,21 @@
+package de.tbias25.urorianetwork;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class UroriaNetwork extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
